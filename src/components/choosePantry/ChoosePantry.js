d963941aea2c4f35fd0f0c3afe872583ed889b2b
Ben Starke <bstarke0225@gmail.com>
2021-03-22 20:10:40 -0400

    cleaned up choose pantry page.js and css

--- src/components/choosePantry/ChoosePantry.js
+++ src/components/choosePantry/ChoosePantry.js
@@ -4,6 +4,17 @@ import './ChoosePantry.css'
 
 export default class ChoosePantry extends Component {
 
+    constructor() {
+        super();
+        this.state = {showMessage: false}
+    }
+
+    _showTable = (bool) => {
+        this.setState({
+                          showMessage: bool
+                      });
+    };
+
     render() {
         return (
             <div className="main-container" id="choosePantry">
@@ -30,8 +41,48 @@ export default class ChoosePantry extends Component {
                                 </select>
                             </div>
                             <div className="btn-search-pantry">
-                                <button type="button" className="btn btn-success">Search</button>
+                                <button type="button" className="btn btn-success"
+                                        onClick={this._showTable.bind(null, true)}>Search
+                                </button>
                             </div>
+                        </div>
+                        <div className="row results-list">
+                            {this.state.showMessage && (
+                                <div className="col-10">
+                                    <div className="list-group" id="list-tab" role="tablist">
+                                        <button type="button"
+                                                className="list-group-item list-group-item-action pantry-option">
+                                            The Greater Boston Food Bank
+                                        </button>
+                                        <button type="button"
+                                                className="list-group-item
+                                        list-group-item-action pantry-option">
+                                            St. Anthony Shrine / Franciscian Food Center
+                                        </button>
+                                        <button type="button"
+                                                className="list-group-item list-group-item-action pantry-option">
+                                            Salvation Army / South End Food Bank
+                                        </button>
+                                        <button type="button"
+                                                className="list-group-item list-group-item-action pantry-option">
+                                            Haley House Food Pantry
+                                        </button>
+                                    </div>
+                                </div>
+                                // <div className="list-group">
+                                //     <button type="button"
+                                //             className="list-group-item list-group-item-action ">
+                                //         The Greater Boston Food Bank
+                                //     </button>
+                                //     <button type="button"
+                                //             className="list-group-item
+                                // list-group-item-action">St. Anthony Shrine / Franciscian Food
+                                // Center </button> <button type="button"
+                                // className="list-group-item list-group-item-action">Salvation
+                                // Army / South End Food Bank </button> <button type="button"
+                                // className="list-group-item list-group-item-action">Haley House
+                                // Food Pantry </button> </div>
+                            )}
                         </div>
                     </div>
                 </div>
