a34110881e5a6806d3b6f8b7b67ac25f1301a215
Ben Starke <bstarke0225@gmail.com>
2021-03-22 21:58:49 -0400

    implemented list component

---
+++ src/components/chooseDonationType/ChooseDonationType.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Link} from 'react-router-dom'
+
+export default class ChooseDonationType extends Component {
+
+    render() {
+        return (
+    <div>
+
+    </div>
+        )
+    }
+}
--- src/components/choosePantry/ChoosePantry.js
+++ src/components/choosePantry/ChoosePantry.js
@@ -1,12 +1,20 @@
 import React, {Component} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './ChoosePantry.css'
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 export default class ChoosePantry extends Component {
 
-    constructor() {
-        super();
-        this.state = {showMessage: false}
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            showMessage: false,
+            showModal: false
+        };
+
+        this.open = this.open.bind(this);
+        this.close = this.close.bind(this);
     }
 
     _showTable = (bool) => {
@@ -14,6 +22,33 @@ export default class ChoosePantry extends Component {
                           showMessage: bool
                       });
     };
+
+    open() {
+        this.setState({showModal: true});
+    }
+
+    close() {
+        this.setState({showModal: false});
+    }
+
+    pantryDetails = (props) => {
+        const unreadMessages = props.unreadMessages;
+        return (
+            <div>
+                <h1>Hello!</h1>
+                {unreadMessages.length > 0 &&
+                 <h2>
+                     You have {unreadMessages.length} unread messages.
+                 </h2>
+                }
+            </div>
+        );
+    }
+
+    getText = (props) => {
+        const text = props;
+
+    }
 
     render() {
         return (
@@ -50,23 +85,122 @@ export default class ChoosePantry extends Component {
                             {this.state.showMessage && (
                                 <div className="col-10">
                                     <div className="list-group" id="list-tab" role="tablist">
-                                        <button type="button"
-                                                className="list-group-item list-group-item-action pantry-option">
-                                            The Greater Boston Food Bank
-                                        </button>
-                                        <button type="button"
-                                                className="list-group-item
-                                        list-group-item-action pantry-option">
-                                            St. Anthony Shrine / Franciscian Food Center
-                                        </button>
-                                        <button type="button"
-                                                className="list-group-item list-group-item-action pantry-option">
-                                            Salvation Army / South End Food Bank
-                                        </button>
-                                        <button type="button"
-                                                className="list-group-item list-group-item-action pantry-option">
-                                            Haley House Food Pantry
-                                        </button>
+                                        <ul className="list-group">
+                                            <li className="list-group-item">
+                                                <div className="row button-row">
+                                                    <div className="col-6 pantry-name">
+                                                        <h5>The Greater Boston Food Bank</h5>
+                                                    </div>
+                                                    <div className="col-6">
+                                                        <Button type="button"
+                                                                className="btn btn-primary details-btn"
+                                                                onClick={this.open}>See
+                                                            Details
+                                                        </Button>
+                                                        <Button type="button"
+                                                                className="btn btn-success details-btn">Choose</Button>
+                                                        <div>
+                                                            <Modal className="modal-container"
+                                                                   show={this.state.showModal}
+                                                                   onHide={this.close}
+                                                                   animation={true}
+                                                                   bsSize="small">
+                                                                <Modal.Header closeButton>
+                                                                    <Modal.Title>Pantry
+                                                                        Details</Modal.Title>
+                                                                </Modal.Header>
+                                                                <Modal.Body>
+                                                                    This pantry is located at 123
+                                                                    Address St., Boston MA, 02130
+                                                                    <br/>
+                                                                    Hours: M-F 9-5
+                                                                </Modal.Body>
+                                                                <Modal.Footer>
+                                                                    <Button
+                                                                        onClick={this.close}>Close</Button>
+                                                                </Modal.Footer>
+                                                            </Modal>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                            <li className="list-group-item">
+                                                <div className="row button-row">
+                                                    <div className="col-6 pantry-name">
+                                                        <h5>St. Anthony Shrine / Franscician Food
+                                                            Bank</h5>
+                                                    </div>
+                                                    <div className="col-6">
+                                                        <Button type="button"
+                                                                className="btn btn-primary details-btn"
+                                                                onClick={this.open}>See
+                                                            Details
+                                                        </Button>
+                                                        <Button type="button"
+                                                                className="btn btn-success details-btn">Choose</Button>
+                                                        <div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                            <li className="list-group-item"> <div className="row button-row">
+                                                <div className="col-6 pantry-name">
+                                                    <h5>Salvation Army / South End Food Bank</h5>
+                                                </div>
+                                                <div className="col-6">
+                                                    <Button type="button"
+                                                            className="btn btn-primary details-btn"
+                                                            onClick={this.open}>See
+                                                        Details
+                                                    </Button>
+                                                    <Button type="button"
+                                                            className="btn btn-success details-btn">Choose</Button>
+                                                    <div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            </li>
+                                            <li className="list-group-item"> <div className="row button-row">
+                                                <div className="col-6 pantry-name">
+                                                    <h5>Haley House Food Pantry</h5>
+                                                </div>
+                                                <div className="col-6">
+                                                    <Button type="button"
+                                                            className="btn btn-primary details-btn"
+                                                            onClick={this.open}>See
+                                                        Details
+                                                    </Button>
+                                                    <Button type="button"
+                                                            className="btn btn-success details-btn">Choose</Button>
+                                                    <div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            </li>
+                                        </ul>
+
+                                        {/*<button type="button"*/}
+                                        {/*        className="list-group-item list-group-item-action pantry-option">*/}
+                                        {/*    <div className="col-6">*/}
+                                        {/*        The Greater Boston Food Bank*/}
+                                        {/*    </div>*/}
+                                        {/*    <div className="col-6">*/}
+
+                                        {/*    </div>*/}
+                                        {/*</button>*/}
+                                        {/*<button type="button"*/}
+                                        {/*        className="list-group-item*/}
+                                        {/*list-group-item-action pantry-option">*/}
+                                        {/*    St. Anthony Shrine / Franciscian Food Center*/}
+                                        {/*</button>*/}
+                                        {/*<button type="button"*/}
+                                        {/*        className="list-group-item list-group-item-action pantry-option">*/}
+                                        {/*    Salvation Army / South End Food Bank*/}
+                                        {/*</button>*/}
+                                        {/*<button type="button"*/}
+                                        {/*        className="list-group-item list-group-item-action pantry-option">*/}
+                                        {/*    Haley House Food Pantry*/}
+                                        {/*</button>*/}
                                     </div>
                                 </div>
                                 // <div className="list-group">
