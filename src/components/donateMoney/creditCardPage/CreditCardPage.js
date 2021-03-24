57c4c137262b88090adfaf74c150beeb114ec5c4
Ben Starke <bstarke0225@gmail.com>
2021-03-24 16:48:14 -0400

    adding credit card form

--- src/components/donateMoney/creditCardPage/CreditCardPage.js
+++ src/components/donateMoney/creditCardPage/CreditCardPage.js
@@ -5,7 +5,6 @@ import './CreditCardPage.css'
 import logo from "../../images/food-pantry-logo.jpg";
 import creditCardsLong from "../../images/credit-cards-long.png"
 
-
 export default class CreditCardPage extends Component {
 
     render() {
@@ -18,7 +17,94 @@ export default class CreditCardPage extends Component {
                         <div className="row">
                             <div className="pantry-info-container container-fluid">
                                 <img src={creditCardsLong} alt={"Credit Cards"} height="80rem"/>
-
+                                <br/>
+                            </div>
+                            <br/>
+                            <div className="container-fluid cc-info">
+                                <div className="row">
+                                    <div className="col-6">
+                                        <form>
+                                            <div className="col-auto">
+                                                <label className="amount-label"><h5>Amount</h5>
+                                                </label>
+                                                <div className="input-group mb-2">
+                                                    <div className="input-group-prepend">
+                                                        <div className="input-group-text">
+                                                            $
+                                                        </div>
+                                                    </div>
+                                                    <input type="text" className="form-control"
+                                                           id="inlineFormInputGroup"
+                                                           placeholder="Enter amount..."/>
+                                                </div>
+                                            </div>
+                                            <br/>
+                                            <div className="col-auto">
+                                                <label className="amount-label"><h5>Cardholder
+                                                    Name</h5></label>
+                                                <div className="input-group mb-2">
+                                                    <input type="text" className="form-control"
+                                                           id="inlineFormInputGroup"
+                                                           placeholder="Enter name..."/>
+                                                </div>
+                                            </div>
+                                            <br/>
+                                            <div className="col-auto">
+                                                <label className="amount-label"><h5>Credit Card
+                                                    Number</h5></label>
+                                                <div className="input-group mb-2">
+                                                    <input type="text" className="form-control"
+                                                           id="inlineFormInputGroup"
+                                                           placeholder="Enter Card Number..."/>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div className="col-6">
+                                        <form>
+                                            <div className="row container">
+                                                <div className="col-auto">
+                                                    <label className="amount-label"><h5>Expiration Date</h5>
+                                                    </label>
+                                                    <div className="input-group mb-2">
+                                                        <input type="text" className="form-control"
+                                                               id="inlineFormInputGroup"
+                                                               placeholder="MM/YYYY"/>
+                                                    </div>
+                                                </div>
+                                                <div className="col-auto">
+                                                    <label className="amount-label"><h5>Security Code</h5>
+                                                    </label>
+                                                    <div className="input-group mb-2">
+                                                        <input type="text" className="form-control"
+                                                               id="inlineFormInputGroup"
+                                                               placeholder="XXX"/>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <br/>
+                                            <div className="col-auto">
+                                                <label className="amount-label"><h5>Cardholder
+                                                    Name</h5></label>
+                                                <div className="input-group mb-2">
+                                                    <input type="text" className="form-control"
+                                                           id="inlineFormInputGroup"
+                                                           placeholder="Enter name..."/>
+                                                </div>
+                                            </div>
+                                            <br/>
+                                            <div className="col-auto">
+                                                <label className="amount-label"><h5>Credit Card
+                                                    Number</h5></label>
+                                                <div className="input-group mb-2">
+                                                    <input type="text" className="form-control"
+                                                           id="inlineFormInputGroup"
+                                                           placeholder="Enter Card NUmber..."/>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
                                 {/*<h2>Pantry you chose: XXX</h2>*/}
                                 {/*<div className="change-pantry-container">*/}
                                 {/*    <Link to="/choosePantry"*/}
