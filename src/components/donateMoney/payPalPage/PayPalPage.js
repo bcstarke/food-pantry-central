881f83c777e4efb84bb548f058ba4b5f24c6eb7f
Ben Starke <bstarke0225@gmail.com>
2021-03-25 21:00:25 -0400

    did paypal page

--- src/components/donateMoney/payPalPage/PayPalPage.js
+++ src/components/donateMoney/payPalPage/PayPalPage.js
@@ -5,6 +5,8 @@ import {Link} from "react-router-dom";
 import '@fortawesome/react-fontawesome'
 import './PayPalPage.css'
 import paypal from "../../images/paypal-transparent.png"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowLeft, faArrowRight} from "@fortawesome/free-solid-svg-icons";
 
 export default class PayPalPage extends Component {
     state = {
@@ -33,7 +35,14 @@ export default class PayPalPage extends Component {
                             <div className="col-12">
                                 <form>
                                     <div className="col-auto">
-                                        <label className="amount-label"><h5>Amount</h5>
+                                        <label className="amount-label"><h3>Email:</h3>
+                                        </label>
+                                        <div className="input-group mb-2">
+                                            <input type="text" className="form-control"
+                                                   id="inlineFormInputGroup"
+                                                   placeholder="Please enter your email..."/>
+                                        </div>
+                                        <label className="amount-label"><h3>Amount</h3>
                                         </label>
                                         <div className="input-group mb-2">
                                             <div className="input-group-prepend">
@@ -43,20 +52,51 @@ export default class PayPalPage extends Component {
                                             </div>
                                             <input type="text" className="form-control"
                                                    id="inlineFormInputGroup"
-                                                   placeholder="Enter amount..."
+                                                   placeholder="Please enter an amount..."
                                                    onChange={this.handleInput}/>
                                         </div>
                                     </div>
                                     {/*<br/>*/}
+                                    <hr/>
                                     <div className="row">
-                                        <div className="col-6">
+                                        <div className="col-4">
                                             <div>
-                                                <b>Amount: </b>
+                                                <h4><b>Amount: </b></h4>
                                             </div>
                                         </div>
-                                        <div className="col-6">
+                                        <div className="col-5"></div>
+                                        <div className="col-3">
                                             <div>
-                                               {this.state.amount}
+                                               ${this.state.amount}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-4">
+                                            <div>
+                                                <h4><b>Fee: </b></h4>
+                                            </div>
+                                        </div>
+                                        <div className="col-5"></div>
+                                        <div className="col-3">
+                                            <div>
+                                                $0
+                                                {/*{this.state.amount}*/}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <hr/>
+                                    <div className="row">
+                                        <div className="col-4">
+                                            <div>
+                                                <h4><b>You Pay: </b></h4>
+                                            </div>
+                                        </div>
+                                        <div className="col-5"></div>
+                                        <div className="col-3">
+                                            <div>
+                                                ${this.state.amount}
+                                                {/*{this.state.amount}*/}
                                             </div>
                                         </div>
                                     </div>
@@ -64,21 +104,35 @@ export default class PayPalPage extends Component {
                             </div>
                         </div>
                         <br/>
-                        {/*<div className="row">*/}
-                        {/*    <div className="col-12">*/}
-                        {/*        <div>*/}
-                        {/*            {this.state.amount}*/}
-                        {/*        </div>*/}
-                        {/*    </div>*/}
-                        {/*</div>*/}
+
                     </div>
+                    <div className="col-12 button-row">
+                        <div className="col-3">
+                            <Link to="/donateMoney"
+                                  class="btn btn-dark btn-lg"
+                                  type="button">
+                                <div className="button-text-cc-page">
+                                    <div className="left-arrow-icon">
+                                        <FontAwesomeIcon icon={faArrowLeft} size='2x'/>
+                                    </div>
+                                    Go Back
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="col-3">
+                            <Link to="/thankYouPayment"
+                                  className="btn btn-success btn-lg cc-page-button"
+                                  type="button">
+                                <div className="button-text-cc-page">
+                                    Donate
+                                    <div className="right-arrow-icon">
+                                        <FontAwesomeIcon icon={faArrowRight} size='2x'/>
+                                    </div>
+                                </div>
 
-                    {/*<div className="row col-12">*/}
-
-                    {/*<div className="img-row">*/}
-                    {/*    <img src={barcode} alt={"Bar Code"} width="380rem"/>*/}
-                    {/*</div>*/}
-                    {/*</div>*/}
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
